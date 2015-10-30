ce110a2d50f0bb69d1417aa0a4714a65eb0f3b3a
AnnaZemtsova <zemtsova18anna@gmail.com>
2015-10-30 14:55:27 +0200

    it is make Anna

---
+++ src/ChatApp2/MessageCommand.java
@@ -0,0 +1,32 @@
+package ChatApp2;
+
+public class MessageCommand extends Command{
+	public static final String message = "Message";
+	
+	private String sendedMessage;
+	private boolean isHaveMessage;
+	
+	public MessageCommand (){
+		super.nowCommand = message;
+	}
+	
+	public void setCommand(String command){
+		if (message.equals(command)) super.isHaveFalseCommand = false;
+		else super.isHaveFalseCommand = true;
+	}
+	
+	public void setMessage(String sendedMessage){
+		 this.sendedMessage = sendedMessage;
+		 this.isHaveMessage = true;
+	}
+	
+	public boolean isHaveMessage(){
+		return isHaveMessage;
+	}
+	
+	public String getMessage(){
+		isHaveMessage = false;
+		return sendedMessage;
+	}
+	
+}
