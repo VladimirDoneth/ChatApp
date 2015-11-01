d63fc5e3c32e8cca62ec40654278da32bef81aff
AnnaZemtsova <zemtsova18anna@gmail.com>
2015-11-01 03:33:57 +0200

    it class is started gui

---
+++ src/ChatApp2/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by anna on 12.11.15.
+ */
+public class Main {
+    public static void main(String[]args){
+        //GUIForm guiUser = new GUIForm();
+        new ChatAppGUI();
+    }
+}
