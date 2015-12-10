6c67873f22bddc9614a3f318acbca46ab15229b7
Донец Владимир <vovan.s.marsa@gmail.com>
2015-12-10 21:28:54 +0200

    эта штуковина сделана вручную на эклипсе

--- src/ChatApp2/InputConnection.java
+++ src/ChatApp2/InputConnection.java
@@ -1,57 +1,58 @@
+package Laba4_plus;
+
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-/**
- * Created by anna on 17.11.15.
- */
-/*public class InputConnection {
-    private JButton acceptButton;
-    private JButton rejectButton;
-    private JLabel InputLabel;
+public class InputConnection {
+    private JFrame frame = new JFrame();
+    private JButton acceptButton = new JButton();
+    private JButton rejectButton = new JButton();
+    private JLabel inputCalling = new JLabel();
+    private JPanel panel = new JPanel();
+    static boolean flag;
+    static boolean isInputConnection = true;
 
-    import javax.swing.*;
-    import java.awt.event.ActionEvent;
-    import java.awt.event.ActionListener;
-
-    /**
-     * Created by anna on 16.11.15.
-     */
-    public class InputConnection {
-        //private JPanel panel;
-        private JButton acceptButton;
-        private JButton rejectButton;
-    private JLabel InputConnectionLabel;
-    private JFrame frame;
-        private GUIForm guiForm;
-        static boolean flag;
-        Connection connection = new Connection();
-
-        public InputConnection(){
-            flag = false;
-            // frame.setContentPane(panel);
-            frame.setSize(300, 100);
-            frame.setLocationRelativeTo(null);
-            frame.setResizable(false);
-            frame.setVisible(true);
-
-
-            acceptButton.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    flag = true;
-                    guiForm.isHaveSocket = true;
-
-                }
-            });
-
-            rejectButton.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    connection.sendMessage("Sorry, i don`t want correspond with you ");
-                }
-            });
-        }
+    public static void main(String args []){
+        new InputConnection("lala");
     }
+    
+    public InputConnection(String name){
+    	isInputConnection = true;
+        frame.setContentPane(panel);
+        frame.setSize(300, 150);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setVisible(true);
+        Box boxh = Box.createHorizontalBox();
+        Box boxv = Box.createVerticalBox();
+        acceptButton.setText("Accept");
+        rejectButton.setText("Reject");
+        boxh.add(acceptButton);
+        boxh.add(Box.createHorizontalGlue());
+        boxh.add(rejectButton);
+        inputCalling.setText(name);
+        boxv.add(inputCalling);
+        boxv.add(boxh);
+        
+        frame.add(boxv,BorderLayout.CENTER);
 
-
+        acceptButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+             flag=true;
+             isInputConnection = false;
+             frame.setVisible(false);
+            }
+        });
+        rejectButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+             flag=false;
+             isInputConnection = false;
+             frame.setVisible(false);
+            }
+        });
+    }
+}
